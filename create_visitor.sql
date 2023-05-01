87900276ef30d42d03003c04bb4478008b775a9b
Amir <70907990+Amirka2@users.noreply.github.com>
2023-05-01 15:36:19 +0500

    added trainer_id for visitor

--- create_visitor.sql
+++ create_visitor.sql
@@ -1,7 +1,9 @@
 CREATE TABLE IF NOT EXISTS Visitor(
     id INTEGER PRIMARY KEY,
+    trainer_id INTEGER NOT NULL,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
     birthday DATE NOT NULL,
-    phone_number VARCHAR(20) NOT NULL
+    phone_number VARCHAR(20) NOT NULL,
+    FOREIGN KEY (trainer_id) REFERENCES Trainer(id)
 );
